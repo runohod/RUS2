7add11bf8eaacbab922c45d4627db68fe70baa99
runohod <andrei.rumynthev1728@yandex.ru>
2024-09-07 19:39:37 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const galleryTrack = document.querySelector('.gallery-track');
+const galleryItems = document.querySelectorAll('.gallery-item');
+const galleryTrack2 = document.querySelector('.gallery-track-two');
+
+function cloneItems() {
+    galleryItems.forEach(item => {
+        const clone = item.cloneNode(true);
+        galleryTrack.appendChild(clone);
+        galleryTrack2.appendChild(clone);
+
+    });
+}
+
+cloneItems();
+
+// const galleryTrack2 = document.querySelector('.gallery-track-two');
+//
+// function cloneItems() {
+//     galleryItems.forEach(item => {
+//         const clone = item.cloneNode(true);
+//         galleryTrack2.appendChild(clone);
+//     });
+// }
+//
+// cloneItems();
